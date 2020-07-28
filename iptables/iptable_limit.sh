e317ff7bcc9b28ea14f278263764a77128b1ea0d
GBOyang <gboyanghao@163.com>
2020-07-28 17:32:51 +0800

    first commit

--- iptables/iptable_limit.sh
+++ iptables/iptable_limit.sh
@@ -8,18 +8,18 @@
 /sbin/iptables -A INPUT -s 127.0.0.1 -d 127.0.0.1 -j ACCEPT
 /sbin/iptables -A INPUT -s 127.0.0.1 -j ACCEPT
 /sbin/iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-/sbin/iptables -A INPUT -s 192.168.1.111 -j ACCEPT
+/sbin/iptables -A INPUT -s 192.168.1.57 -j ACCEPT
 /sbin/iptables -A INPUT -s 192.168.1.112 -j ACCEPT
 /sbin/iptables -A INPUT  -p tcp --dport 22  -j ACCEPT
 /sbin/iptables -A INPUT  -p tcp --dport 80  -j ACCEPT
-/sbin/iptables -A INPUT -s 192.168.56.101 -m limit --limit 2400/s --limit-burst 10 -j ACCEPT 
-/sbin/iptables -A INPUT -s 192.168.56.101 -j DROP
-/sbin/iptables -A FORWARD -d 192.168.56.101 -m limit --limit 2400/s --limit-burst 10 -j ACCEPT 
-/sbin/iptables -A FORWARD -d 192.168.56.101 -j DROP
-/sbin/iptables -A FORWARD -s 192.168.56.101 -m limit --limit 2400/s --limit-burst 10 -j ACCEPT 
-/sbin/iptables -A FORWARD -s 192.168.56.101 -j DROP
-/sbin/iptables -A OUTPUT  -s 192.168.56.101 -m limit --limit 2400/s --limit-burst 10 -j ACCEPT 
-/sbin/iptables -A OUTPUT  -s 192.168.56.101 -j DROP
+/sbin/iptables -A INPUT -s 192.168.1.50 -m limit --limit 2400/s --limit-burst 10 -j ACCEPT 
+/sbin/iptables -A INPUT -s 192.168.1.50 -j DROP
+/sbin/iptables -A FORWARD -d 192.168.1.50 -m limit --limit 2400/s --limit-burst 10 -j ACCEPT 
+/sbin/iptables -A FORWARD -d 192.168.1.50 -j DROP
+/sbin/iptables -A FORWARD -s 192.168.1.50 -m limit --limit 2400/s --limit-burst 10 -j ACCEPT 
+/sbin/iptables -A FORWARD -s 192.168.1.50 -j DROP
+/sbin/iptables -A OUTPUT  -s 192.168.1.50 -m limit --limit 2400/s --limit-burst 10 -j ACCEPT 
+/sbin/iptables -A OUTPUT  -s 192.168.1.50 -j DROP
 /sbin/iptables -A INPUT -j REJECT
 /sbin/iptables -A FORWARD -j REJECT
 /sbin/iptables -A OUTPUT -j ACCEPT
